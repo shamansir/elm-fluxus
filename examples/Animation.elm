0e48e7555078fdc06b7f904d36bcf65be217901d
Ulric Wilfred <shaman.sir@gmail.com>
2017-02-02 10:40:12 +0100

    animation works now!

--- examples/Animation.elm
+++ examples/Animation.elm
@@ -10,8 +10,8 @@ drawRow count state =
         state
             -- |> color (vec3 (toFloat count / 10) 0 0)
             |> drawCube
-            -- |> rotate (vec3 0 0 (45 * (sin (time state))))
-            |> rotate (vec3 0 0 45)
+            |> rotate (vec3 0 0 (45 * (sin (time state))))
+            -- |> rotate (vec3 0 0 45)
             |> translate (vec3 2.5 0 0)
             |> drawRow (count - 1)
     else
